01c9d8e2b60694c775d6a99cfc3fb6f568c0fb9b
SeungM <rarang222@gmail.com>
2022-05-27 07:48:04 -0700

    test_first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, redirect, render_template, jsonify
+from models import db, connect_db, Users
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sqla_info'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+
+@app.route("/")
+def list_info():
+    users = Users.query.all()
+    return render_template("list.html", users=users) 
+
+@app.route("/" , methods = ["POST"])
+def add_user():
+
+    name = request.form['name']
+    address = request.form['address']
+    users = Users(name=name, address=address)
+    db.session.add(users)
+    db.session.commit()
+
+    return redirect(f"/{users.id}")
+
+@app.route("/<int:users_id>" )
+def show_user(users_id):
+    # users = Users.get_or_404(users_id)
+    users = Users.query.filter_by(id=users_id).first_or_404()
+    return render_template("detail.html", users=users)
+
+
+@app.route("/<int:users_id>/delete" , methods=["GET","POST"])
+def delete_user(users_id):
+    users = Users.query.filter_by(id=users_id).one()
+    # users = Users.query.get(users_id)
+    if request.method == "POST" :
+        if users:
+            db.session.delete(users)
+            db.session.commit()
+            return redirect('/')
+
+    return render_template("delete.html", users=users) 
+    # return "Successfully deleted"
---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class Users(db.Model):
+    __tableName__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50),nullable=False, unique=True)
+    address =  db.Column(db.String(50),nullable=True, unique=True)
+
